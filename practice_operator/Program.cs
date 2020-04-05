c4fc864fe9fc9cb4afefb81a1936c95435b2d7db
mai1121 <maimaiukawa@gmail.com>
2020-04-05 22:15:04 +0900

    first commit

---
+++ practice_operator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
